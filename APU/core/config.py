fd18e47472224228bfeb24b4ba9551568ff580c9
Andrea Malloni <malloni.andrea02@gmail.com>
2022-11-09 22:46:11 +0100

    introduced keymapping

--- APU/core/config.py
+++ APU/core/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pygame as pg
 
 TILE_SIZE = 16
 
@@ -7,13 +8,16 @@ D_HEIGHT = 720
 
 IDLE = 0
 NORTH = 1
-NORTH_EAST = 2
-EAST = 3
-SOUTH_EAST = 4
-SOUTH = 5
-SOUTH_WEST = 6
-WEST = 7
-NORTH_WEST = 8
+EAST = 2
+SOUTH = 3
+WEST = 4
 
 DEFAULT_FPS = 60
 ASSETSPATH = f"{Path.cwd()}\\assets"
+
+KEYMAP = {
+    pg.K_w: NORTH,
+    pg.K_a: WEST,
+    pg.K_s: SOUTH,
+    pg.K_d: EAST
+}
