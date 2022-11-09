3e6a1acb6f455acb34473d252d4ee42e61ab2981
Andrea Malloni <malloni.andrea02@gmail.com>
2022-11-09 22:45:10 +0100

    typehint changes

--- APU/entityObject.py
+++ APU/entityObject.py
@@ -3,7 +3,7 @@ from APU.core.spritesheet import SpriteStripAnim
 import pygame as pg
 
 class EntityObject(MovingObject):
-    def __init__(self, x:int, y:int, defaultSpriteImage:pg.Surface = pg.Surface((0, 0)), speed = 1.0, hp = 10, **kwargs:SpriteStripAnim):
+    def __init__(self, x:int, y:int, defaultSpriteImage:pg.Surface = pg.Surface((0, 0)), speed:int = 1, hp:int = 10, **kwargs:SpriteStripAnim):
         super().__init__(x, y, defaultSpriteImage, speed, **kwargs)
         self.hp = hp
 
