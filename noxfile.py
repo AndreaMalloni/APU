b4a7f241c730ee960d4ca004a1195ee7f38be7ec
Andrea Malloni <malloni.andrea02@gmail.com>
2025-08-10 09:10:17 +0200

    Missing python-version file

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ nox.options.default_venv_backend = "uv"
 nox.options.sessions = ["lint", "type_check", "test", "docs"]
 
 
-@session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])  # type: ignore[misc]
+@session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])  
 def test(s: Session) -> None:
     s.run_install(
         "uv",
@@ -34,8 +34,8 @@ def test(s: Session) -> None:
 
 # For some sessions, set venv_backend="none" to simply execute scripts within the existing
 # uv-generated virtual environment, rather than have nox create a new one for each session.
-@session(venv_backend="none")  # type: ignore[misc]
-@parametrize(  # type: ignore[misc]
+@session(venv_backend="none")
+@parametrize(
     "command",
     [
         param(
@@ -61,8 +61,8 @@ def fmt(s: Session, command: list[str]) -> None:
     s.run(*command)
 
 
-@session(venv_backend="none")  # type: ignore[misc]
-@parametrize(  # type: ignore[misc]
+@session(venv_backend="none")
+@parametrize(
     "command",
     [
         param(["ruff", "check", "."], id="lint_check"),
@@ -73,12 +73,12 @@ def lint(s: Session, command: list[str]) -> None:
     s.run(*command)
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def lint_fix(s: Session) -> None:
     s.run("ruff", "check", ".", "--extend-fixable", "F401", "--fix")
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def type_check(s: Session) -> None:
     s.run("mypy", "src", "tests")
 
@@ -87,32 +87,32 @@ def type_check(s: Session) -> None:
 doc_env = {"PYTHONPATH": "src"}
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def docs(s: Session) -> None:
     s.run("mkdocs", "build", env=doc_env)
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def docs_check_urls(s: Session) -> None:
     s.run("mkdocs", "build", env=doc_env | {"HTMLPROOFER_VALIDATE_EXTERNAL_URLS": str(True)})
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def docs_offline(s: Session) -> None:
     s.run("mkdocs", "build", env=doc_env | {"MKDOCS_MATERIAL_OFFLINE": str(True)})
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def docs_serve(s: Session) -> None:
     s.run("mkdocs", "serve", env=doc_env)
 
 
-@session(venv_backend="none")  # type: ignore[misc]
+@session(venv_backend="none")
 def docs_github_pages(s: Session) -> None:
     s.run("mkdocs", "gh-deploy", "--force", env=doc_env)
 
 
-@session  # type: ignore[misc]
+@session
 def licenses(s: Session) -> None:
     # Generate a unique temporary file name. uv cannot write to the temp file directly on
     # Windows, so only use the name and allow uv to re-create it.
